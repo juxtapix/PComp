0b564d87795269c616525e7378b5658400be0810
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2024-09-23 17:40:57 -0400

    Create CheckAnalogVoltage.ino

---
+++ examples/013_CheckAnalogInput/CheckAnalogVoltage.ino
@@ -0,0 +1,19 @@
+// PComp 2023
+// Analog Input - CheckAnalogVoltage
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);           // initialize serial (console) at 9600 bps.
+                                // you don't need to initialize analog pins.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (3.3 / 1024.0);  // convert value to voltage. 
+  
+  Serial.print( sensorValue );          // print the value of the analog pin.
+  Serial.print(" | " );
+  Serial.println( voltage );            // print the voltage of the analog pin.
+  delay(100);                           // delay in between reads for stability
